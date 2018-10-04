969d724f5c3162912183ab64ef389cb463d81b2d
Ashiq <ashiq08401@gmail.com>
2018-10-04 01:38:49 -0400

    HTML Forms and JavaScript

--- Lab 3/wwwroot/formCalc.js
+++ Lab 3/wwwroot/formCalc.js
@@ -7,12 +7,10 @@
         dateDisplay.style.visibility = "hidden";
     }
 }
-
 var ageCalc = function () {
     var date = document.querySelector("#date").value;
     var bDay = moment(date);
     var min = moment().subtract("21", 'years');
-
     if (bDay.isAfter(min)) {
         document.querySelector("#output1").innerHTML = "Must be born before ";
         document.querySelector("#output2").innerHTML = min.format("dddd, MMMM, D, YYYY");
@@ -24,11 +22,18 @@ var ageCalc = function () {
         return true;
     }
 }
-
-var prices = [[2.5, 4, 5], [2.5, 5.5, 7.5], [4, 7, 8], [6, 8, 9]];
+var prices = [
+    [2.5, 4, 5],
+    [2.5, 5.5, 7.5],
+    [4, 7, 8],
+    [6, 8, 9]
+];
 var beverages = ["Coffee", "Tea", "Beer", "Wine"];
-var sizes = { 0: "Small", 1: "Medium", 2: "Large" };
-
+var sizes = {
+    0: "Small",
+    1: "Medium",
+    2: "Large"
+};
 var printReceipt = function () {
     var name = document.querySelector("#name").value;
     var drink = document.querySelector("select option:checked").value;
@@ -37,7 +42,6 @@ var printReceipt = function () {
     var quantity = Number(document.querySelector("#amount").value);
     var price = prices[drink][size];
     var totalPrice = quantity * price;
-
     if (ageCalc()) {
         document.querySelector("#output1").innerHTML = "Customer: " + name;
         document.querySelector("#output2").innerHTML = quantity + " " + sizes[size] + " " + drinkName + " @ $" + price;
@@ -46,12 +50,10 @@ var printReceipt = function () {
         document.querySelector("#receipt").style.background = 'white';
     }
 }
-
 window.onload = function () {
     document.querySelector("#bDay").style.visibility = "hidden";
     document.querySelector("#receipt").style.visibility = "hidden";
     var drop = document.querySelector("#dropdown");
     drop.onchange = ageCheck;
     document.querySelector("#btn").onclick = printReceipt;
-
 }
