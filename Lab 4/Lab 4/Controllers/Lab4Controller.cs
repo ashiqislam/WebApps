febc3e1415e50c4f6532cc477c690b0b5d3b2086
Ashiq <ashiq08401@gmail.com>
2018-10-11 04:04:27 -0400

    Intro to MVC

---
+++ Lab 4/Lab 4/Controllers/Lab4Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_4.Controllers
+{
+    public class Lab4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+       
+            DateTime date = DateTime.Now;
+            DateTime endDate = new DateTime(2019, 1, 1);
+            TimeSpan span = endDate.Subtract(date);
+            ViewBag.DaysLeft = span.Days;
+
+            ViewBag.Greeting = "";
+
+            if (DateTime.Now.Hour < 12)
+            {
+                ViewBag.Greeting = "Good Morning";
+            }
+            else if (DateTime.Now.Hour < 18)
+            {
+                ViewBag.Greeting = "Good Afternoon";
+            }
+            else
+            {
+                ViewBag.Greeting = "Good Evening";
+            }
+
+            return View(date);
+        }
+
+    }
+}
